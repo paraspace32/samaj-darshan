ee3ed0c73e6a5a052a6dcdf668e6f0109ead18b4
Paras Asati <paraspace3@gmail.com>
2026-04-05 09:05:56 +0530

    ui upgraded

---
+++ app/javascript/controllers/ticker_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["track"]
+
+  connect() {
+    this.paused = false
+    this.speed = 0.5
+    this.start()
+  }
+
+  disconnect() {
+    this.stop()
+  }
+
+  start() {
+    this.frame = requestAnimationFrame(() => this.tick())
+  }
+
+  stop() {
+    if (this.frame) cancelAnimationFrame(this.frame)
+  }
+
+  tick() {
+    if (!this.paused) {
+      const track = this.trackTarget
+      track.scrollLeft += this.speed
+      if (track.scrollLeft >= track.scrollWidth - track.clientWidth) {
+        track.scrollLeft = 0
+      }
+    }
+    this.frame = requestAnimationFrame(() => this.tick())
+  }
+
+  pause() { this.paused = true }
+  resume() { this.paused = false }
+}
